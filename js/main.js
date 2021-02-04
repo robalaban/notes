23e9d635e30547f4bcf434eae9ee95f51624180d
robalaban <rob.balaban@gmail.com>
2021-02-04 23:47:39 +0200

    fix: dark theme on fast refresh - move to css variables

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
+let root = document.documentElement;
+
 const userPrefersDark =
     window.matchMedia &&
     window.matchMedia("(prefers-color-scheme: dark)").matches;
 
-if (userPrefersDark) {
-    localStorage.setItem("userThemePreference", "dark");
+if (userPrefersDark || localStorage.getItem("themeColor") === "dark") {
+    root.style.setProperty("--main-bg-color", "#1f2937");
+    root.style.setProperty("--main-text-color", "rgba(209, 213, 219, 1)");
+    root.style.setProperty("--main-accent-color", "rgba(31, 41, 55, 1)");
 } else {
-    localStorage.setItem("userThemePreference", "light");
+    root.style.setProperty("--main-bg-color", "#f9fafb");
+    root.style.setProperty("--main-text-color", "rgba(55, 65, 81, 1)");
+    root.style.setProperty("--main-accent-color", "rgba(219, 39, 119, 1)");
 }
 
 function theme() {
@@ -19,9 +25,11 @@ function theme() {
             let currentTheme = localStorage.getItem("themeColor");
             if (currentTheme === "dark") {
                 localStorage.setItem("themeColor", "light");
+                root.style.setProperty("--main-bg-color", "#f9fafb");
                 this.themeClass = "";
             } else {
                 localStorage.setItem("themeColor", "dark");
+                root.style.setProperty("--main-bg-color", "#1f2937");
                 this.themeClass = "dark";
             }
         },
---
+++ site/notes/one_month_startup_retrospective.md
@@ -0,0 +1,20 @@
+---
+title: One month retrospective on building my startup
+excerpt: 2020 has been "the year of change" for most of us, one of the biggest career moves I've made this year is deciding to quit my job and go freelance.
+date: 2021-01-31
+tags:
+    - note
+    - realtalk
+---
+
+Has it already been a month. That's my general state of though, time seams to be speeding up when stuck being at home and your only window to society has become through the internet.
+
+At the begging of the month I've proposed to myself to make January the month that kickstarts my startup journey, but this hasn't been without kicks, so I figured to keep me accountable I'm giong to write about the journey and share it with the internet. See, saying and drawing a plan is quite different than executing one.
+
+On startup vs contracting.
+
+I quit my day job back in December 2020, I took some time off and quickly realised I had to support myself if I was going to make my dream come true. There are generally two ways of approaching this:
+
+-   Contracting, leverage your network and apply your skills
+
+to get contracts to support myself ( this whole startup thing would have been )
